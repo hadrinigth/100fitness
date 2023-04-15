969479230af861b63d7b7be63bde692a03477d7c
HAdriney <hadrineytaylors@gmail.com>
2023-04-15 14:53:52 -0300

    base create

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import * as pageController from "../controllers/pageController"
+
+
+const router = Router();
+router.get("/", pageController.home)
+
+
+
+export default router;
